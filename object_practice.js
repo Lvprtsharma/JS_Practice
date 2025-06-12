8788727dd15e2f62c6596beca443bf29701ee209
Lovepreet Sharma <lvprtsharma@gmail.com>
2025-06-12 10:14:33 +0530

    Added Practice

---
+++ object_practice.js
@@ -0,0 +1,19 @@
+const profile = {
+
+    name: "Lovepreet",
+    posts: 195,
+    followers:569000,
+    following: 10,
+    isFollow: false,
+
+}
+
+console.log(profile);
+
+console.log(profile.name);
+console.log(profile.posts);
+console.log(profile.followers);
+console.log(profile.following);
+console.log(profile.isFollow);
+
+console.log(typeof profile);
---
+++ primitive_datatypes.js
@@ -0,0 +1,48 @@
+/*
+
+Primitive Data Types:
+1. Number
+2. String
+3. Boolean
+4. Null
+5. Undefined
+6. Symbol
+7. BigInt
+
+*/
+
+// Number
+let agenum = 20;
+console.log(agenum);
+console.log(typeof agenum);
+
+// String
+let fullname = "Lovepreet";
+console.log(fullname);
+console.log(typeof fullname);
+
+// Boolean
+let isStudent = true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+// Null
+let a = null;
+console.log(a);
+console.log(typeof a);
+
+// Undefined
+let b;
+console.log(b);
+console.log(typeof b);
+
+// Symbol
+let c = Symbol("Lovepreet");
+console.log(c);
+console.log(typeof c);
+
+// BigInt
+let d = 123456789012345678901234567890n;
+console.log(d);
+console.log(typeof d);
+
